86fd932bc3c98d2f06fe145b9062de9d6ab44f34
SmitaCodeHub <smitapatil29@gmail.com>
2023-02-18 14:49:16 +0800

    First Commit

---
+++ One-One -Association-Mapping/src/in/iNeuron/bean/Account.java
@@ -0,0 +1,18 @@
+package in.iNeuron.bean;
+
+public class Account {
+	String accNo;
+	String accName;
+	String accType;
+	
+	
+	public Account(String accNo, String accName, String accType) {
+		super();
+		this.accNo   = accNo;
+		this.accName = accName;
+		this.accType = accType;
+	}
+	
+	
+
+}
